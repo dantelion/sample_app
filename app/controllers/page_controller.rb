0fbb68fec414c7a0833685bb23d1660e521b1a59
nicolas <p1e5@free.fr>
2014-10-26 16:11:15 +0100

    fin des pages statiques

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,14 @@
 class PageController < ApplicationController
+  
   def home
+    @titre = "Accueil"
   end
 
   def contact
+    @titre = "Contact"
+  end
+
+  def about
+    @titre = "a Propos"
   end
 end
